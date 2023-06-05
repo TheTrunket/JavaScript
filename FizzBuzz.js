e0ce884a2f39d6095c112785e7eae929f6639b32
TheTrunket <thetrinket01@gmail.com>
2023-06-05 20:33:50 +0200

    swapped 3 and 5

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -8,11 +8,11 @@ function fizzBuzz() {
     if (count%3 == 0 && count%5 == 00){
         output.push("FizzBuzz");
      
-    } else if (count%5 == 0){
-        output.push("Buzz");
-        
-} else if (count%3 == 0 ) {
+    } else if (count%3 == 0){
         output.push("Fizz");
+        
+} else if (count%5 == 0 ) {
+        output.push("Buzz");
         
 } else output.push(count);
 
